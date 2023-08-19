cf19d38815ef4451ffb71290c60c7b283d477737
adryellefelippe <adryellefelippe@outlook.com>
2023-08-19 11:14:41 -0300

    Alterações no .js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const apiConector = (event) => {
+document.querySelector('#botao').addEventListener('click', (event)=> {
     event.preventDefault();
 
-    fetch('https://api.sheetmonkey.io/form/ayv7gRJe8g28LVuZGUumLx'), {
+    var problema = document.querySelector('select[name=problema]').value;
+    var descricao = document.querySelector('textarea[name=descricao]').value;
+    var cep = document.querySelector('input[name=cep').value;
+    var bairro = document.querySelector('input[name=bairro]').value;
+    var rua = document.querySelector('input[name=rua]').value;
+    var data_abertura = new Date().toLocaleDateString();
 
-        method: 'post',
+    fetch('https://api.sheetmonkey.io/form/ayv7gRJe8g28LVuZGUumLx', {
+        method: 'POST',
         headers: {
-            'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-
-        }
-        )
-    }
+        body: JSON.stringify({problema, descricao, cep, bairro, rua, data_abertura}),
+    });
 
     alert('enviado');
-}
-
-document.querySelector('form').addEventListener('submit', apiConector)
+});

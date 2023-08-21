95fbf8f3881af709f12f3a827da0d374d3228ff0
adryellefelippe <adryellefelippe@outlook.com>
2023-08-21 20:27:58 -0300

    Adição de imagens, animação de loading e alterações no .js

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
+var button = document.querySelector('#botao');
+var inputs = document.querySelectorAll('input');
+var selects = document.querySelector('select');
+var textAreas = document.querySelector('textarea');
+
+function checkInputs (inputs, select, textArea){
+    var filled = true;
+
+    inputs.array.forEach(input => {
+        if(input.value === ''){
+            filled = false;
+        }
+    });
+
+    if(select.value === ''){
+        filled = false;
+    };
+
+    if (textArea.value === ''){
+        filled = false;
+    };
+
+    return filled;
+};
+
+function addLoading() {
+    button.innerHTML = '<img src="images/load.png" alt="Loading" class="loading">';
+};
+
+function remoceLoading (){
+    button.innerHTML = 'Enviar';
+};
+
+inputs.forEach(input => {
+    input.addEventListener('keyup', () => {
+        if (checkInputs(inputs, selects, textAreas) === true){
+            button.disabled = false;
+        } else {
+            button.disabled = true;
+        };
+    });
+});
+
 document.querySelector('#botao').addEventListener('click', (event)=> {
     event.preventDefault();
+    addLoading();
 
     var problema = document.querySelector('select[name=problema]').value;
     var descricao = document.querySelector('textarea[name=descricao]').value;
@@ -14,7 +58,13 @@ document.querySelector('#botao').addEventListener('click', (event)=> {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({problema, descricao, cep, bairro, rua, data_abertura}),
-    });
+    }).then(() => {
+        remoceLoading();
 
-    alert('enviado');
+        document.querySelector('select[name=problema]').value = '';
+        document.querySelector('textarea[name=descricao]').value = '';
+        document.querySelector('input[name=cep').value = '';
+        document.querySelector('input[name=bairro]').value = '';
+        document.querySelector('input[name=rua]').value = '';
+    });
 });

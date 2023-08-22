17ab42685acf6724536f9f679959d1b6e9589b0b
adryellefelippe <adryellefelippe@outlook.com>
2023-08-22 20:41:27 -0300

    Alterações para não enviar o form vazio

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 var button = document.querySelector('#botao');
-var inputs = document.querySelectorAll('input');
-var selects = document.querySelector('select');
-var textAreas = document.querySelector('textarea');
+var fields = document.querySelectorAll('[required]');
+var vazio;
 
-function checkInputs (inputs, select, textArea){
-    var filled = true;
-
-    inputs.array.forEach(input => {
-        if(input.value === ''){
-            filled = false;
-        }
-    });
-
-    if(select.value === ''){
-        filled = false;
-    };
-
-    if (textArea.value === ''){
-        filled = false;
-    };
-
-    return filled;
-};
+console.log(fields);
 
 function addLoading() {
     button.innerHTML = '<img src="images/load.png" alt="Loading" class="loading">';
@@ -31,40 +12,50 @@ function remoceLoading (){
     button.innerHTML = 'Enviar';
 };
 
-inputs.forEach(input => {
-    input.addEventListener('keyup', () => {
-        if (checkInputs(inputs, selects, textAreas) === true){
-            button.disabled = false;
-        } else {
-            button.disabled = true;
-        };
-    });
-});
+for (field of fields){
+    field.addEventListener('invalid', event => {
+        console.log('campo inválido');
+    })
+}
 
 document.querySelector('#botao').addEventListener('click', (event)=> {
     event.preventDefault();
     addLoading();
 
-    var problema = document.querySelector('select[name=problema]').value;
-    var descricao = document.querySelector('textarea[name=descricao]').value;
-    var cep = document.querySelector('input[name=cep').value;
-    var bairro = document.querySelector('input[name=bairro]').value;
-    var rua = document.querySelector('input[name=rua]').value;
-    var data_abertura = new Date().toLocaleDateString();
-
-    fetch('https://api.sheetmonkey.io/form/ayv7gRJe8g28LVuZGUumLx', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({problema, descricao, cep, bairro, rua, data_abertura}),
-    }).then(() => {
-        remoceLoading();
-
-        document.querySelector('select[name=problema]').value = '';
-        document.querySelector('textarea[name=descricao]').value = '';
-        document.querySelector('input[name=cep').value = '';
-        document.querySelector('input[name=bairro]').value = '';
-        document.querySelector('input[name=rua]').value = '';
+    fields.forEach(element => {
+        if(element.value === null || element.value === ''){
+            vazio = true;
+        } else {
+            vazio = false;
+        }
     });
+
+    if(vazio === false){
+        var problema = document.querySelector('select[name=problema]').value;
+        var descricao = document.querySelector('textarea[name=descricao]').value;
+        var cep = document.querySelector('input[name=cep').value;
+        var bairro = document.querySelector('input[name=bairro]').value;
+        var rua = document.querySelector('input[name=rua]').value;
+        var data_abertura = new Date().toLocaleDateString();
+        
+        fetch('https://api.sheetmonkey.io/form/ayv7gRJe8g28LVuZGUumLx', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({problema, descricao, cep, bairro, rua, data_abertura}),
+        }).then(() => {
+            remoceLoading();
+    
+            document.querySelector('select[name=problema]').value = '';
+            document.querySelector('textarea[name=descricao]').value = '';
+            document.querySelector('input[name=cep').value = '';
+            document.querySelector('input[name=bairro]').value = '';
+            document.querySelector('input[name=rua]').value = '';
+        });
+    } else {
+        remoceLoading();
+        alert('campos vazios');
+    }
+
 });
